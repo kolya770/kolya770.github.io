721a3b24542b32f9211dc20c2a46f1f16a8b7f4a
kolya770 <grinko.kolya770@gmail.com>
2016-02-18 14:59:57 +0200

    other-pages v1

--- js/jAccordion.js
+++ js/jAccordion.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+
     $(".accordion > .nav__btn").click(function(){
+
         $(".nav__text").removeClass('nav__item--active');
+
         if(false == $(this).next().is(':visible')) {
+
             $('.accordion > ul').stop().slideUp(400);
             $(this).addClass('nav__item--active');
         }
+
         $(this).next().stop().slideToggle(400);
     });
+
     var link = $('.sub-nav__link--active').parent();
+
     $(link).parent().attr('id', 'sub-nav--active');
     $('#sub-nav--active').prev().addClass("nav__item__two--active");
 });
