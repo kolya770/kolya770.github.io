b2cb2d81aa61e162d4ebcabe405617ca3bd2f9f1
kolya770 <grinko.kolya770@gmail.com>
2016-02-26 07:24:57 +0200

    navbar+ js animation

--- js/jFade.js
+++ js/jFade.js
@@ -1,3 +1,9 @@
-$(window).fadeThis({speed:1000});
+$(document).ready(function() {
+    $('.first-screen-animation')
+        .animate({
+         bottom: "+=10vh",
+         opacity: 1
+        }, 1500);
 
-$('.container').fadeThis({speed:1000});
+    $('.container').fadeThis({speed: 1000});
+});
