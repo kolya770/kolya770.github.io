684862dfe99d9bb27dd84b4da217a7cef3abb083
kolya770 <grinko.kolya770@gmail.com>
2016-03-01 16:32:56 +0200

    new version site v1

--- js/jFade.js
+++ js/jFade.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
-    $('.first-screen-animation').animate({
-         bottom: "+=10vh",
-         opacity: 1
-        }, 1500);
 
-    $('.fade-bottom').fadeThis({
-        speed: 1000
-    });
+    var md = new MobileDetect(window.navigator.userAgent);
+
+    if ( ! md.mobile()) {
+
+        $('.fade-bottom').fadeThis({
+            speed: 1000
+        });
+
+        $('.first-screen-animation').animate({
+            bottom: "+=10vh",
+            opacity: 1
+        }, 1500);
+    }
 
 });
