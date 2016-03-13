600b6f41586e5466fe1e97e89389a64c2bb89083
kolya770 <grinko.kolya770@gmail.com>
2016-03-13 14:16:13 +0200

    history-of-orders

--- js/Accordion.js
+++ js/Accordion.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
 
     var lastIcon;
 
+    // FAQ ACCORDION
+
     $('.accordion > .accordion-item').click(function () {
 
         if ($(this).hasClass('active')) {
@@ -17,5 +19,24 @@ $(document).ready(function () {
         $(this).find('i').add(lastIcon).toggleClass('fa-angle-down fa-angle-up');
         lastIcon = $(this).find('i');
     });
+
+    //HISTORY-OF-ORDERS ACCORDION
+
+    $('.accordion-order > .accordion-item').click(function () {
+
+        if ($(this).hasClass('active')) {
+
+            $(this).removeClass('active');
+            $(this).find('i').toggleClass('fa-angle-double-down fa-angle-double-up');
+            lastIcon = null;
+            return true;
+        }
+
+        $('.accordion-order > .accordion-item').removeClass('active');
+        $(this).addClass('active');
+        $(this).find('i').add(lastIcon).toggleClass('fa-angle-double-down fa-angle-double-up');
+        lastIcon = $(this).find('i');
+    });
+
 });
 
