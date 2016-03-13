ad15710f768b1f4518910ac50cf5bd2178b13e02
Svyatoslav <kiev.devel@gmail.com>
2016-03-13 02:39:35 +0100

    night fixes

--- js/main.js
+++ js/main.js
@@ -9,23 +9,23 @@ $(document).ready(function() {
     });
 
     $(function(){
-
+        // subscription management dropdowns
         $("#days-drop .dropdown-menu li a").click(function(){
-
             $('#days-drop button')[0].childNodes[0].nodeValue=($(this).text());
-
         });
 
         $("#month-drop .dropdown-menu li a").click(function(){
-
             $('#month-drop button')[0].childNodes[0].nodeValue=($(this).text());
-
         });
 
         $("#time-drop .dropdown-menu li a").click(function(){
-
             $('#time-drop button')[0].childNodes[0].nodeValue=($(this).text());
+        });
 
+
+
+        $('.question-icon').tooltip({
+            delay: { "show": 50, "hide": 200 }
         });
 
     });
