ed883a5b0f9af22c42110605c6ca4c4627056d8c
kolya770 <grinko.kolya770@gmail.com>
2016-03-14 23:34:47 +0200

    password js html cs

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
+    alert(window.location.hash);
+    if (window.location.hash == "#passwordUpdated") {
+        //bootstrap show popup JS
+        $('#passwordUpdated').modal({});
+        alert(window.location.hash);
+
+    }
 
     $('#go-to-step-2').on('click', function(){
         location.href = 'address.html';
@@ -22,12 +29,86 @@ $(document).ready(function() {
             $('#time-drop button')[0].childNodes[0].nodeValue=($(this).text());
         });
 
-
-
         $('.question-icon').tooltip({
             delay: { "show": 50, "hide": 200 }
         });
 
     });
+
+    $('#send-email').on('click', function(){
+
+        var send_text =  $('#email').val();
+
+        $('#your-mail').html(send_text);
+
+        $('.fade-password').fadeOut("slow", function() {});
+
+        setTimeout(function(){
+            $('.modal-sended-email').fadeIn("slow");
+
+        }, 750);
+
+
+    });
+
+    $('.new-password').bind('keyup change tap', function() {
+
+        var password =  $('#password');
+        var confirm_password =  $('#confirm_password');
+
+        if (password.val() != confirm_password.val()) {
+
+            password.css('border-color', 'red');
+            confirm_password.css('border-color', 'red');
+
+        } else {
+
+            password.css('border-color', '#cbcbcb');
+            confirm_password.css('border-color', '#cbcbcb');
+
+        }
+    });
+
+
+
+
+    //var password = document.getElementById("password")
+    //    , confirm_password = document.getElementById("confirm_password");
+    //
+    //function validatePassword(){
+    //    if(password.value != confirm_password.value) {
+    //        confirm_password.setCustomValidity("Passwords Don't Match");
+    //    } else {
+    //        confirm_password.setCustomValidity('');
+    //    }
+    //}
+    //
+    //password.onchange = validatePassword;
+    //confirm_password.onkeyup = validatePassword;
+
+    //$('#change-pass').on('click', function() {
+
+        //var password =  $('#first-pass').val();
+        //var confirm_password =  $('#second-pass').val();
+        //
+        //
+        //
+        //if (newpass == copypass) {
+        //    $('#show-ok-message').fadeIn("slow");
+        //
+        //
+        //} else {
+        //    $('#error-message').fadeIn("slow");
+        //
+        //}
+
+        //$('.fade-pass').fadeOut("slow", function() {});
+        //
+        //setTimeout(function(){
+        //    $('.modal-change-pass').fadeIn("slow");
+        //
+        //}, 750);
+
+    //});
 
 });
