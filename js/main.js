e7bef9c7faf17fe56acc363aaed385485625be40
kolya770 <grinko.kolya770@gmail.com>
2016-03-18 17:41:16 +0200

    js fix 6

--- js/main.js
+++ js/main.js
@@ -158,7 +158,7 @@ $(document).ready(function() {
         });
 
         //store drop
-        $("#item-1 .dropdown-menu li a").on('tap', function() {
+        $("body").on('tap', function() {
             alert(1);
             $('#item-1 button').html(($(this).html() + '<i class="fa fa-sort-desc item-arrow"></i>'));
         });
